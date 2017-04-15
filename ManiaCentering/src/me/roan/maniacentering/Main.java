603d46ba2fbbe23d761b240ef7ea29b40c78dcef
Roan <roan.hofland@hotmail.com>
2017-04-15 14:19:35 +0200

    Add the option to directly parse files

--- ManiaCentering/src/me/roan/maniacentering/Main.java
+++ ManiaCentering/src/me/roan/maniacentering/Main.java
@@ -11,12 +11,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -27,6 +37,7 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.UIManager;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * Simple program that can be used
@@ -66,7 +77,7 @@ public class Main {
 		dim.add(h);
 		
 		//sum
-		JPanel sum = new JPanel(new GridLayout(3, 1, 0, 2));
+		JPanel sum = new JPanel(new GridLayout(4, 1, 0, 2));
 		sum.setBorder(BorderFactory.createTitledBorder("Column sum"));
 		ButtonGroup group = new ButtonGroup();
 		JRadioButton rsum = new JRadioButton();
@@ -103,9 +114,31 @@ public class Main {
 		product.add(n2, BorderLayout.LINE_END);
 		pcal.add(product, BorderLayout.LINE_END);
 		
+		JButton file = new JButton("Directly apply to file");
+		file.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser();
+				chooser.setFileFilter(new FileNameExtensionFilter("INI files", "ini"));
+				chooser.setMultiSelectionEnabled(false);
+				chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+				if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+					try {
+						parse(chooser.getSelectedFile(), (int)height.getValue(), (int)width.getValue());
+						JOptionPane.showMessageDialog(null, "ColumnStart values succesfully added!", "Mania Centering", JOptionPane.ERROR_MESSAGE);
+					} catch (IOException e1) {
+						e1.printStackTrace();
+						JOptionPane.showMessageDialog(null, "An error occured!", "Mania Centering", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+		});
+		
 		sum.add(psum);
 		sum.add(pcol);
 		sum.add(pcal);
+		sum.add(file);
 		
 		//result
 		JPanel pval = new JPanel(new BorderLayout());
@@ -170,6 +203,55 @@ public class Main {
 	private static final void calculate(double screenHeight, double screenWidth, double sum){
 		int value = (int) (((480.0D / (screenHeight / screenWidth)) / 2.0D) - (sum / 2.0D));
 		val.setText("ColumnStart: " + value);
+	}
+	
+	/**
+	 * Parses a file and applies the column start value
+	 * @param ini The file to modify
+	 * @param screenHeight The screen height
+	 * @param screenWidth The screen width
+	 * @throws IOException When an IOException occurs
+	 */
+	private static final void parse(File ini, double screenHeight, double screenWidth) throws IOException{
+		File tmp = Files.createTempFile(null, null).toFile();
+		PrintWriter out = new PrintWriter(new FileOutputStream(tmp));
+		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(ini)));
+		
+		String line;
+		while((line = in.readLine()) != null){
+			if(line.startsWith("ColumnStart:")){
+				out.println("//" + line);
+			}else if(line.startsWith("ColumnWidth:")){
+				try{
+					String[] vals = line.replaceAll("ColumnWidth:", "").replaceAll(" ", "").split(",");
+					int sum = 0;
+					for(String val : vals){
+						sum += Integer.parseInt(val);
+					}
+					out.println(line);
+					out.println("ColumnStart: " + (int) (((480.0D / (screenHeight / screenWidth)) / 2.0D) - (sum / 2.0D)));
+				}catch(Exception e){
+					e.printStackTrace();
+					JOptionPane.showMessageDialog(null, "An error occured!", "Mania Centering", JOptionPane.ERROR_MESSAGE);
+					out.close();
+					in.close();
+					tmp.delete();
+					tmp.deleteOnExit();
+					return;
+				}
+			}else{
+				out.println(line);
+			}
+		}
+		
+		out.flush();
+		out.close();
+		in.close();
+		
+		Files.move(tmp.toPath(), ini.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		
+		tmp.delete();
+		tmp.deleteOnExit();
 	}
 	
 	/**
